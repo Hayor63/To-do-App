cc893fed2d3f95915d03d6817b95294f81ef2b75
hayor63 <hayor63@gmail.com>
2025-02-01 11:18:13 +0100

    deployment

--- config/default.ts
+++ config/default.ts
@@ -7,10 +7,5 @@ export default {
       ? process.env.TEST
       : process.env.DB,
   accessTokenPrivateKey: process.env.ACCESSTOKEN,
-  refreshTokenPrivateKey: process.env.REFRESHTOKEN,
-  BREVO_MAIL_KEY: process.env.BREVO_MAIL_KEY,
-  HOST: process.env.HOST,
-  BREVO_PORT: process.env.BREVO_PORT,
-  USER_MAIL_LOGIN: process.env.USER_MAIL_LOGIN,
-  BASE_URL: process.env.BASE_URL, // ✅ Ensure this line exists
+  refreshTokenPrivateKey: process.env.REFRESHTOKEN
 };
